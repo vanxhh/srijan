91dac43355d2edb8ae191b72a69486b044227db2
Vansh Bhardwaj <71017319+vanxhh@users.noreply.github.com>
2023-03-26 21:04:43 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
+  assetPrefix: isProd ? 'srijan' : '',
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
 }
